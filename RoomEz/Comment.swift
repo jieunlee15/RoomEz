11a3999d98cd0b2297fb0ca84d76156d5418bef7
svenkataraman108 <166966428+svenkataraman108@users.noreply.github.com>
2025-12-08 17:15:47 -0600

    adding

---
+++ RoomEz/Comment.swift
@@ -0,0 +1,11 @@
+// Comment.swift
+
+import Foundation
+
+struct Comment {
+    let id: String
+    let text: String
+    let author: String
+    let timestamp: Date
+    let replyToAuthor: String? // This is the new field for replies
+}
---
+++ RoomEz/CommentsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  CommentsViewController.swift
+//  RoomEz
+//
+//  Created by Venkataraman, Shriya on 12/8/25.
+//
+
+import Foundation
